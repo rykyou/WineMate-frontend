eda3d321d06f2eb7bdfbee814bf62c6a5ec134d9
rykyou <rkim1214@gmail.com>
2019-02-24 23:16:12 -0500

    Initial try to render Winestyle with highest score on ResultsPage

--- src/containers/ResultsPage.js
+++ src/containers/ResultsPage.js
@@ -4,14 +4,32 @@ import React, { Component } from 'react';
 
 class ResultsPage extends Component {
 
+  wineIdWithHighestScore = () => {
+    let highestScore = 0;
+    let highestScoreWineId = 1;
+    this.props.finalScoresArray().forEach(wineInfo => {
+      if (wineInfo.finalScore > highestScore) {
+        highestScoreWineId = wineInfo.wine_id;
+        highestScore = wineInfo.finalScore;
+      }
+    })
+    return highestScoreWineId;
+  }
 
+  wineObjWithHighestScore = () => {
+    return this.props.allWineStyles.find(wineStyle => wineStyle.id === this.wineIdWithHighestScore())
+  }
 
   render() {
     console.log(this.props.finalScoresArray())
+    console.log(this.wineIdWithHighestScore())
+    console.log(this.wineObjWithHighestScore())
+    const wineStyle = this.wineObjWithHighestScore()
     return (
       <div>
         <h1>ResultsPage</h1>
-
+        <h3>Here's your perfect wine style match for your meal!</h3>
+        <h4>{wineStyle.name}</h4>
       </div>
     )
   }
