d004c758699ae91929e73c5d471066255b1fee70
rykyou <rkim1214@gmail.com>
2019-03-25 21:20:37 -0400

    Start adding in background circle images to InfographicDialog

--- src/components/InfographicDialog.js
+++ src/components/InfographicDialog.js
@@ -5,6 +5,7 @@ import {AppBar, Button, Dialog, DialogContent, Grid, IconButton,
 import CloseIcon from '@material-ui/icons/Close';
 import infographicImage from '../images/wine-folly-infographic.png';
 import LargerInfographicDialog from './LargerInfographicDialog';
+import circle from '../images/circle-red.png';
 
 const styles = theme => ({
   appBar: {
@@ -28,7 +29,14 @@ const styles = theme => ({
   },
   textBottomMargin: {
     marginBottom: '3vh'
-  }
+  },
+  circle: {
+    backgroundImage: 'url(' + circle + ')',
+    backgroundSize: 'contain',
+    height: '100%',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'left'
+  },
 });
 
 function Transition(props) {
@@ -78,7 +86,7 @@ class InfographicDialog extends React.Component {
               alignItems="center"
               justify="center"
             >
-              <Grid item md={12} lg={5}>
+              <Grid item md={5}>
                 <Button onClick={this.handleShowLargerImage}>
                   <img
                     src={infographicImage}
@@ -87,35 +95,37 @@ class InfographicDialog extends React.Component {
                   />
                 </Button>
               </Grid>
-              <Grid item md={12} lg={7}>
+              <Grid item md={7}>
                 <Typography variant="h4" className={classes.title}>
                   Simplifying the Science of Food and Wine Pairing
                 </Typography>
-                <Typography variant="subheading" className={classes.textBottomMargin}>
-                  Traditionally, there are two ways to understand wines: by variety (e.g. Sauvignon Blanc or Syrah) or by region (e.g. Barossa or Bordeaux).
-                  However, with so many different angles to approach wine, how do you get started?
-                  Fortunately, you can start classifying grape varieties by these 9 broader styles!
-                </Typography>
-                <Typography variant="subheading" className={classes.textBottomMargin}>
+                <div className={classes.circle}>
+                  <Typography variant="body1" className={classes.textBottomMargin}>
+                    Traditionally, there are two ways to understand wines: by variety (e.g. Sauvignon Blanc or Syrah) or by region (e.g. Barossa or Bordeaux).
+                    However, with so many different angles to approach wine, how do you get started?
+                    Fortunately, you can start classifying grape varieties by these 9 broader styles!
+                  </Typography>
+                </div>
+                <Typography variant="body1" className={classes.textBottomMargin}>
                   Borrowed from Wine Folly's easy-to-use pairing chart, our algorithm makes finding the perfect wine style easy. The perfect wine style match for your dinner (lunch or breakfast - shhh we'll keep it a secret) is only a quick survey away.
                 </Typography>
                 <Typography variant="h5" className={classes.subTitle}>
                   Why do certain wines go with certain foods?
                 </Typography>
-                <Typography variant="subheading" className={classes.textBottomMargin}>
+                <Typography variant="body1" className={classes.textBottomMargin}>
                   When you start analyzing the structure of wine, each type of wine features different characteristics such as acidity, tannin, alcohol level and sweetness. If you start thinking about wine traits as flavor ingredients, it becomes easier to pair them with a meal.
                 </Typography>
                 <Typography variant="h5" className={classes.subTitle}>
                   How it works in action...
                 </Typography>
-                <Typography variant="subheading" className={classes.textBottomMargin}>
+                <Typography variant="body1" className={classes.textBottomMargin}>
                   When it comes to food and wine pairing, most folks lean on the phrase “What grows together, goes together” as a starting point.
                   For example, you could pair Italian Sangiovese with Italian pasta and make a decent pairing without trying.
                 </Typography>
                 <Typography variant="h5" className={classes.subTitle}>
                   Example of how the pairing works
                 </Typography>
-                <Typography variant="subheading" className={classes.textBottomMargin}>
+                <Typography variant="body1" className={classes.textBottomMargin}>
                   Let's take fish tacos as an example and break them down to their core ingredients. The fish turns out to be a pretty polarizing ingredient that doesn’t usually pair with red wines. Additionally, cilantro and lime will push this dish closer to a much more specific wine.
 
                   If you follow the chart, you’ll see that a light-bodied white wine looks to be the best option for this dish. And, it is! Of the wines on the list shown, you’ll do great with a Vermentino, Albariño, or Pinot Grigio.
