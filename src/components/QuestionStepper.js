f4caf67b2dcf2c4066f240415f39da51a7a54438
rykyou <rkim1214@gmail.com>
2019-04-16 15:40:19 -0400

    Clean up QuestionStepper

--- src/components/QuestionStepper.js
+++ src/components/QuestionStepper.js
@@ -4,8 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepButton from '@material-ui/core/StepButton';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   root: {
@@ -30,66 +28,9 @@ function getSteps() {
   return ['Meat', 'Veggies', 'Starch', 'Dairy', 'Herbs', 'Prep'];
 }
 
-// function getStepContent(step) {
-//   switch (step) {
-//     case 0:
-//       return 'Step 1: Select campaign settings...';
-//     case 1:
-//       return 'Step 2: What is an ad group anyways?';
-//     case 2:
-//       return 'Step 3: This is the bit I really care about!';
-//     default:
-//       return 'Unknown step';
-//   }
-// }
-
 class QuestionStepper extends React.Component {
   state = {
-    activeStep: 0,
-    completed: new Set(),
-    skipped: new Set(),
-  };
-
-  totalSteps = () => getSteps().length;
-
-  handleSkip = () => {
-    const { activeStep } = this.state;
-    // if (!this.isStepOptional(activeStep)) {
-    //   // You probably want to guard against something like this
-    //   // it should never occur unless someone's actively trying to break something.
-    //   throw new Error("You can't skip a step that isn't optional.");
-    // }
-
-    this.setState(state => {
-      const skipped = new Set(state.skipped.values());
-      skipped.add(activeStep);
-      return {
-        activeStep: state.activeStep + 1,
-        skipped,
-      };
-    });
-  };
-
-  handleNext = () => {
-    let activeStep;
-
-    if (this.isLastStep() && !this.allStepsCompleted()) {
-      // It's the last step, but not all steps have been completed
-      // find the first step that has been completed
-      const steps = getSteps();
-      activeStep = steps.findIndex((step, i) => !this.state.completed.has(i));
-    } else {
-      activeStep = this.state.activeStep + 1;
-    }
-    this.setState({
-      activeStep,
-    });
-  };
-
-  handleBack = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep - 1,
-    }));
+    activeStep: 0
   };
 
   handleStepClick = step => () => {
@@ -99,66 +40,9 @@ class QuestionStepper extends React.Component {
     });
   };
 
-  handleStep = step => () => {
-    this.setState({
-      activeStep: step - 2,
-    });
-  }
-
-  handleComplete = () => {
-    // eslint-disable-next-line react/no-access-state-in-setstate
-    const completed = new Set(this.state.completed);
-    completed.add(this.state.activeStep);
-    this.setState({
-      completed,
-    });
-
-    /**
-     * Sigh... it would be much nicer to replace the following if conditional with
-     * `if (!this.allStepsComplete())` however state is not set when we do this,
-     * thus we have to resort to not being very DRY.
-     */
-    if (completed.size !== this.totalSteps() - this.skippedSteps()) {
-      this.handleNext();
-    }
-  };
-
-  handleReset = () => {
-    this.setState({
-      activeStep: 0,
-      completed: new Set(),
-      skipped: new Set(),
-    });
-  };
-
-  skippedSteps() {
-    return this.state.skipped.size;
-  }
-
-  isStepSkipped(step) {
-    return this.state.skipped.has(step);
-  }
-
-  isStepComplete(step) {
-    return this.state.completed.has(step);
-  }
-
-  completedSteps() {
-    return this.state.completed.size;
-  }
-
-  allStepsCompleted() {
-    return this.completedSteps() === this.totalSteps() - this.skippedSteps();
-  }
-
-  isLastStep() {
-    return this.state.activeStep === this.totalSteps() - 1;
-  }
-
   render() {
     const { classes } = this.props;
     const steps = getSteps();
-    // const { activeStep } = this.state;
 
     return (
       <div className={classes.root}>
@@ -166,14 +50,10 @@ class QuestionStepper extends React.Component {
           {steps.map((label, index) => {
             const props = {};
             const buttonProps = {};
-            if (this.isStepSkipped(index)) {
-              props.completed = false;
-            }
             return (
               <Step key={label} {...props}>
                 <StepButton
                   onClick={this.handleStepClick(index)}
-                  completed={this.isStepComplete(index)}
                   {...buttonProps}
                 >
                   {label}
