298d2b2bb8d3bc12bfa8f20814f2f6cff88cc2f5
rykyou <rkim1214@gmail.com>
2019-04-17 17:00:13 -0400

    Fix algorithm to account for no food item input

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -29,8 +29,12 @@ const styles = theme => ({
   },
   h1: {
     color: theme.palette.primary.main,
-    fontFamily: 'Sriracha, cursive',
+    fontFamily: 'Sriracha',
   },
+  errorMessage: {
+    margin: '40% 5% 10% 5%',
+    fontFamily: 'Sriracha'
+  }
 });
 
 class ResultsPage extends Component {
@@ -43,60 +47,65 @@ class ResultsPage extends Component {
       this.setState({
         query: 'success',
       });
-    }, 2500);
+    }, 2000);
   };
 
-  wineIdWithHighestScore = () => {
+  wineWithHighestScore = () => {
     let highestScore = 0;
-    let highestScoreWineId = 1;
+    let highestScoreWineId = 0;
     this.props.finalScoresArray().forEach(wineInfo => {
       if (wineInfo.finalScore > highestScore) {
         highestScoreWineId = wineInfo.wine_id;
         highestScore = wineInfo.finalScore;
       }
     })
-    return highestScoreWineId;
-  }
-
-  wineObjWithHighestScore = () => {
-    return this.props.allWineStyles.find(wineStyle => wineStyle.id === this.wineIdWithHighestScore())
+    if (highestScore === 0) {
+      return null
+    } else {
+      return this.props.allWineStyles.find(wineStyle => wineStyle.id === highestScoreWineId)
+    }
   }
 
   render() {
     const { classes } = this.props;
     const { query } = this.state;
-    const wineStyle = this.wineObjWithHighestScore()
+    const wineStyle = this.wineWithHighestScore()
     return (
       <div className={classes.root}>
         {query === 'success' ? (
           <div>
-            <Grid className={classes.formTop}>
-              <h2>Here's your perfect wine style match based on your menu...</h2>
-            </Grid>
-            <Grid className={classes.formMiddle}>
-              <Button
-                component={Link}
-                to={`/winestyles/${wineStyle.slug}`}
-                size="large"
-                className={classes.formMiddle}
-              >
-                <Grid container spacing={8}>
-                  <Grid item xs={4}>
-                    <img
-                      src={require(`../images/${wineStyle.slug}.png`)}
-                      alt={wineStyle.name}
-                      className={classes.wineImage}
-                    />
-                  </Grid>
-                  <Grid item xs={7}>
-                    <h1 className={classes.h1}>{wineStyle.name}</h1>
-                    <Typography variant="body1" className={classes.text}>
-                      {wineStyle.short_description}
-                    </Typography>
+              {wineStyle !== null ? (
+                <Grid className={classes.formTop}>
+                  <h2>Here's your perfect wine style match based on your menu...</h2>
+                  <Grid className={classes.formMiddle}>
+                    <Button
+                      component={Link}
+                      to={`/winestyles/${wineStyle.slug}`}
+                      size="large"
+                      className={classes.formMiddle}
+                      >
+                      <Grid container spacing={8}>
+                        <Grid item xs={4}>
+                          <img
+                            src={require(`../images/${wineStyle.slug}.png`)}
+                            alt={wineStyle.name}
+                            className={classes.wineImage}
+                            />
+                        </Grid>
+                        <Grid item xs={7}>
+                          <h1 className={classes.h1}>{wineStyle.name}</h1>
+                          <Typography variant="body1" className={classes.text}>
+                            {wineStyle.short_description}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Button>
                   </Grid>
                 </Grid>
-              </Button>
-            </Grid>
+              ) : (
+                <h2 className={classes.errorMessage}>Sorry, you have to pick at least one food item or prep method for us to match you up with a wine style!</h2>
+              )}
+
             <Button
               variant="contained"
               color="secondary"
