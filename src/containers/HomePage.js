0b2f6949623dd7747458ede8ead04e6a209a4b25
Robin Kim <robin@thinknimble.com>
2020-02-05 23:56:13 -0500

    Fix scroll on wine list

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AnimatedSlider from '../components/AnimatedSlider';
-import WineStylesNavigation from './WineStylesNavigation';
-import HomeNavBar from '../components/HomeNavBar';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Grid, withStyles } from "@material-ui/core";
+import AnimatedSlider from "../components/AnimatedSlider";
+import WineStylesNavigation from "./WineStylesNavigation";
+import HomeNavBar from "../components/HomeNavBar";
 
 const styles = theme => ({
-    button: {
-      width: "250px",
-      height: "150px",
-      marginLeft: "1350px",
-    },
-    empty: {
-      height: "300px"
-    },
-    smallEmpty: {
-      height: "100px"
-    },
-    text: {
-      color: theme.palette.secondary.main,
-      textAlign: 'center',
-      textSize: '20vh',
-      fontFamily: 'Sriracha'
-    }
+  button: {
+    width: "250px",
+    height: "150px",
+    marginLeft: "1350px"
+  },
+  empty: {
+    height: "300px"
+  },
+  smallEmpty: {
+    height: "100px"
+  },
+  text: {
+    color: theme.palette.secondary.main,
+    textAlign: "center",
+    textSize: "20vh",
+    fontFamily: "Sriracha"
+  },
+  navigation: {
+    padding: "2%"
+  }
 });
-
 
 class HomePage extends Component {
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <HomeNavBar handleClickDialog={this.props.handleClickDialog}/>
+        <HomeNavBar handleClickDialog={this.props.handleClickDialog} />
         <AnimatedSlider
           handleClickDialog={this.props.handleClickDialog}
           wineStyleToGoTo={this.props.wineStyleToGoTo}
         />
         <h1 className={classes.text}>Explore by Wine Style</h1>
-        <WineStylesNavigation
-          allWineStyles={this.props.allWineStyles}
-          handleSelectWineStyle={this.props.handleSelectWineStyle}
-          selectedWineStyle={this.props.selectedWineStyle}
-        />
+        <Grid container className={classes.navigation}>
+          <WineStylesNavigation
+            allWineStyles={this.props.allWineStyles}
+            handleSelectWineStyle={this.props.handleSelectWineStyle}
+            selectedWineStyle={this.props.selectedWineStyle}
+          />
+        </Grid>
       </div>
-    )
+    );
   }
 }
 
-
 HomePage.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(HomePage);
--- src/containers/WineStylesNavigation.js
+++ src/containers/WineStylesNavigation.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { BottomNavigation, BottomNavigationAction, withStyles } from '@material-ui/core';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  withStyles
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const styles = {
   root: {
     flexGrow: 1,
-    minHeight: "30vh"
+    flexShrink: 2,
+    minHeight: "30vh",
+    overflowX: "scroll",
+    justifyContent: "unset"
   },
   wineImage: {
     maxHeight: "25vh"
@@ -16,45 +23,52 @@ const styles = {
 
 class WineStylesNavigation extends React.Component {
   state = {
-    value: '',
+    value: ""
   };
 
   componentDidMount() {
-    this.setState({value: this.props.selectedWineStyle})
+    this.setState({ value: this.props.selectedWineStyle });
   }
 
   handleChange = (event, value) => {
     this.setState({ value });
-    this.props.handleSelectWineStyle(value)
+    this.props.handleSelectWineStyle(value);
   };
 
   render() {
     const { classes, allWineStyles } = this.props;
     const { value } = this.state;
     return (
-      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
-        {allWineStyles ?
-          allWineStyles.map(wineStyle => <BottomNavigationAction
-            key={wineStyle.id}
-            component={Link}
-            to={`/winestyles/${wineStyle.slug}`}
-            label={<FavoriteIcon />}
-            value={wineStyle.slug}
-            icon={<img
-              src={require(`../images/${wineStyle.slug}.png`)}
-              alt={wineStyle.slug}
-              className={classes.wineImage}
-            />}
-          />)
-        :
-        null}
+      <BottomNavigation
+        value={value}
+        onChange={this.handleChange}
+        className={classes.root}
+      >
+        {allWineStyles
+          ? allWineStyles.map(wineStyle => (
+              <BottomNavigationAction
+                key={wineStyle.id}
+                component={Link}
+                to={`/winestyles/${wineStyle.slug}`}
+                label={<FavoriteIcon />}
+                value={wineStyle.slug}
+                icon={
+                  <img
+                    src={require(`../images/${wineStyle.slug}.png`)}
+                    alt={wineStyle.slug}
+                    className={classes.wineImage}
+                  />
+                }
+              />
+            ))
+          : null}
       </BottomNavigation>
     );
   }
 }
 
 WineStylesNavigation.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(WineStylesNavigation);
